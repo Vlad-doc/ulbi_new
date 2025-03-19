422521ad05543e8efd2bf49fdb54b82ffa3fd84d
Vlados <vladislav890@gmail.com>
2025-03-19 23:07:22 +0300

    refactor: added test

--- src/widgets/SideBar/ui/Sidebar.tsx
+++ src/widgets/SideBar/ui/Sidebar.tsx
@@ -10,6 +10,10 @@ export const Sidebar = () => {
     const [collapsed, setCollapsed] = useState(false);
     const toggleCollapsed = () => setCollapsed(prev => !prev);
 
+    const test = "a";
+
+    console.log(test);
+
     return (
         <div data-testid="sidebar" className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed })}>
             <button data-testid="sidebar-toggle" onClick={toggleCollapsed}>Collapse</button>
